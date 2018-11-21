44df90681a3403a8dbbfece48b986e3bed7a5253
kratiukkarol <kratiukkarol@gmail.com>
2018-11-21 17:08:05 +0100

    Successfully linked backend with frontend and added some test cases
    Signed-off-by: kratiukkarol <kratiukkarol@gmail.com>

--- shoppingBackend/src/main/java/com/kratiukkarol/shoppingBackend/dao/CategoryDAO.java
+++ shoppingBackend/src/main/java/com/kratiukkarol/shoppingBackend/dao/CategoryDAO.java
@@ -5,9 +5,10 @@ import java.util.List;
 import com.kratiukkarol.shoppingBackend.dto.Category;
 
 public interface CategoryDAO {
-
-	boolean add(Category category);
 	
-	List<Category> list();
 	Category get(int id);
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
 }
--- shoppingBackend/src/main/java/com/kratiukkarol/shoppingBackend/daoImpl/CategoryDAOImpl.java
+++ shoppingBackend/src/main/java/com/kratiukkarol/shoppingBackend/daoImpl/CategoryDAOImpl.java
@@ -1,9 +1,9 @@
 package com.kratiukkarol.shoppingBackend.daoImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,65 +12,56 @@ import com.kratiukkarol.shoppingBackend.dao.CategoryDAO;
 import com.kratiukkarol.shoppingBackend.dto.Category;
 
 @Repository("categoryDAO")
+@Transactional
 public class CategoryDAOImpl implements CategoryDAO {
 	
 	@Autowired
 	private SessionFactory sessionFactory;
-
-	private static List<Category> categories = new ArrayList<>();
-
-	static {
-		Category category = new Category();
-
-		// adding first category
-		category.setId(1);
-		category.setName("Television");
-		category.setDescription("This is some description of television");
-		category.setImageURL("CAT_1.png");
-
-		categories.add(category);
-
-		// adding second category
-		category = new Category();
-		category.setId(2);
-		category.setName("Mobile");
-		category.setDescription("This is some description of mobile");
-		category.setImageURL("CAT_2.png");
-
-		categories.add(category);
-
-		// adding third category
-		category = new Category();
-		category.setId(3);
-		category.setName("Laptop");
-		category.setDescription("This is some description of laptop");
-		category.setImageURL("CAT_3.png");
-
-		categories.add(category);
+	
+	//Getting single category based on id
+	@Override
+	public Category get(int id) {
+		return sessionFactory.getCurrentSession().get(Category.class, Integer.valueOf(id));
 	}
 
 	@Override
 	public List<Category> list() {
-		return categories;
+		String selectActiveCategory = "FROM Category WHERE active = :active";
+		Query query = sessionFactory.getCurrentSession().createQuery(selectActiveCategory);
+		query.setParameter("active", true);
+		return query.getResultList();
 	}
 
 	@Override
-	public Category get(int id) {
-		for (Category category : categories) {
-			if (category.getId() == id)
-				return category;
-		}
-		return null;
-	}
-
-	@Override
-	@Transactional
 	public boolean add(Category category) {
 		try {
 			// add the category to the database table
 			sessionFactory.getCurrentSession().persist(category);
 			return true;
 			
+		} catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(Category category) {
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;		
+		} catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	@Override
+	public boolean delete(Category category) {
+		category.setActive(false);
+		try {
+			sessionFactory.getCurrentSession().update(category);
+			return true;		
 		} catch(Exception e){
 			e.printStackTrace();
 			return false;
