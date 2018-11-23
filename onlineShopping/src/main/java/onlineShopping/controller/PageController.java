5c2e2fa8a181317fe9837e568b198751ee44b787
kratiukkarol <kratiukkarol@gmail.com>
2018-11-23 17:34:41 +0100

    Successfully added slf4j logging dependencies and logger
    Signed-off-by: kratiukkarol <kratiukkarol@gmail.com>

--- onlineShopping/src/main/java/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/onlineShopping/controller/PageController.java
@@ -1,5 +1,7 @@
 package onlineShopping.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import com.kratiukkarol.shoppingBackend.dto.Product;
 @Controller
 public class PageController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
@@ -24,6 +28,9 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
+		
+		logger.info("Inside PageController index method - INFO");
+		logger.debug("Inside PageController index method - DEBUG");
 		
 		//passing the list of categories
 		mv.addObject("categories", categoryDAO.list());
