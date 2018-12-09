f7dec842e6c30c59273ba986aa53803bc660677b
kratiukkarol <kratiukkarol@gmail.com>
2018-12-09 14:07:08 +0100

    Working with spring webflow
    Signed-off-by: kratiukkarol <kratiukkarol@gmail.com>

--- onlineShopping/src/main/java/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/onlineShopping/controller/PageController.java
@@ -108,4 +108,12 @@ public class PageController {
 		
 		return mv;
 	}
+	
+	// having similar mapping to our flow id
+	@RequestMapping(value= {"/register"})
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About us");
+		return mv;
+	}
 }
